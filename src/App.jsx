77e1a47a9363127886b53bc15a4745f60b8c2a0e
Marius <mariuss342@gmail.com>
2025-01-27 20:55:06 +0200

    fixed names order

--- src/App.jsx
+++ src/App.jsx
@@ -2,16 +2,32 @@ import React from 'react';
 import './App.scss';
 import { Person } from './components/Person/Person';
 
-const people = [
-  { name: 'Misha', age: 37, sex: 'm', isMarried: true, partnerName: 'Natasha' },
-  { name: 'Olya', sex: 'f', isMarried: true, partnerName: 'Maksym' },
-  { name: 'Alex', age: 25, sex: 'm', isMarried: false },
-];
+export const misha = {
+  name: 'Misha',
+  age: 37,
+  sex: 'm',
+  isMarried: true,
+  partnerName: 'Natasha',
+};
+
+export const olya = {
+  name: 'Olya',
+  sex: 'f',
+  isMarried: true,
+  partnerName: 'Maksym',
+};
+
+export const alex = {
+  name: 'Alex',
+  age: 25,
+  sex: 'm',
+  isMarried: false,
+};
 
 export const App = () => (
   <div className="App">
-    {people.map((key, iPerson) => (
-      <Person key={iPerson.name} person={iPerson} />
-    ))}
+    <Person person={misha} />
+    <Person person={olya} />
+    <Person person={alex} />
   </div>
 );
